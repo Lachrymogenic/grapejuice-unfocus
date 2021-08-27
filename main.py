a9889c4b5e557881b0f66f85ba102b2d3d59fa8c
Lacrimogenic <liamdoubleday@gmail.com>
2021-08-27 20:03:59 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,37 @@
+from tkinter import *
+from pynput import mouse
+import psutil
+
+def checkIfProcessRunning(processName): #FROM: https://thispointer.com/python-check-if-a-process-is-running-by-name-and-find-its-process-id-pid/
+    '''
+    Check if there is any running process that contains the given name processName.
+    '''
+    #Iterate over the all the running process
+    for proc in psutil.process_iter():
+        try:
+            # Check if process name contains the given name string.
+            if processName.lower() in proc.name().lower():
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return False;
+
+def on_click(x, y, button, pressed):
+    if not pressed and button == mouse.Button.right:
+        root = Tk()
+        root.geometry('2x1')
+ 
+        button = Button(root, text = 'Close the window', command = root.destroy)
+        button.pack(pady = 25)
+        root.update()
+        root.destroy()
+    elif button == mouse.Button.right:
+        #detect if roblox is running
+        if checkIfProcessRunning('RobloxPlayerBet'):
+            pass
+        else:
+            print('No roblox process running, quitting')
+            exit()
+
+with mouse.Listener(on_click=on_click) as listener:
+    listener.join()
